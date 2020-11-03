6c41b46f85c7b06ffa4eb019c2637b65f89fa674
Scott Hallauer <scott.hallauer@gmail.com>
2020-11-03 21:23:52 +0200

    Move copyright message to website footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Sense-i, Inc. Built with Docusaurus.`,
+      copyright: `<span>Copyright © 1998 to ${new Date().getFullYear()} Sense-i Management Systems. All Rights Reserved.</span><span style="display: block; margin-top: 5px; opacity: 0.6;">This process, and all associated documentation, is proprietary and confidential to Sense-i Management Systems. It cannot be used, disclosed or duplicated without the prior written consent of Sense-i. This is an published work protected by copyright laws and no unauthorised copying, adaptation, distribution or display is permitted.</span>`,
     },
   },
   presets: [
