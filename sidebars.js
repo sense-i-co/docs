00181cf477889931ba93ccc41a0141d2b94d18f2
Scott Hallauer <scott.hallauer@gmail.com>
2020-11-05 18:44:17 +0200

    Complete skeleton tree structure for documentation Fill in all categories for the sidebar on the documentation pages. Also add a comment explaining how the sidebar is displayed on the website.

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,22 @@
+/*
+  This represents the tree structure of the sidebar for the documentation
+  section of the website.
+  Note that branches only display on the website if they have child documents 
+  (i.e. leaf nodes). While we finish populating the full tree with actual 
+  documents, I have created a template document (doc ID: XXX-000) to be used 
+  as a placeholder for branches awaiting child documents.
+*/
 module.exports = {
   docs: {
     "Introduction to Sense-i": ["SCL-000"],
     "Level of Work": ["XXX-000"],
     "Business Processes": [{
-      "Operations Management": ["SOE-002"],
+      "Operations Management": [
+        "SOE-002",
+        {"Production Control": ["XXX-000"]},
+        {"Material Cost Control": ["XXX-000"]},
+        {"Labour Cost Control": ["XXX-000"]},
+      ],
       "Financial Management": ["XXX-000"],
       "Performance Management": ["XXX-000"],
     }],
