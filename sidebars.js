003e936eca550f6ec38fd325b84265ffc57e170f
Scott Hallauer <scott.hallauer@gmail.com>
2020-11-10 13:57:39 +0200

    Fix indentation in sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,7 @@ module.exports = {
     "Level of Work": ["XXX-000"],
     "Business Processes": [{
       "Operations Management": [
-		    {"Sales Quote & Order Entry": ["SQO-101","SOE-002","SOE-003"]},
+        {"Sales Quote & Order Entry": ["SQO-101","SOE-002","SOE-003"]},
         {"Production Control": ["XXX-000"]},
         {"Material Cost Control": ["XXX-000"]},
         {"Labour Cost Control": ["XXX-000"]},
