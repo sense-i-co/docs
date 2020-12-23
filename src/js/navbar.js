b8cec117be4c25dc1feb1199cfa61d2ce7489de2
Scott Hallauer <scott.hallauer@gmail.com>
2020-12-23 12:37:50 +0200

    Refactor navbar mutator code into class structure

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,89 +1,88 @@
 import $ from "jquery";
 import location from 'location-href'; 
 
-var NAVBAR_MUTATION = {
+var NavBarMutator = {
+
+  // Attributes
   required: null,
   timer: null,
-  count: 0
+  count: 0,
+
+  // Functions
+  init: function() {
+    if(NavBarMutator.required == null) {
+      if ($("nav").length == 0 && NavBarMutator.timer == null) {
+        NavBarMutator.startTimer(this.init, 100);
+      } else if ($("nav a.dropdown__link[items]").length > 0) {
+        NavBarMutator.stopTimer();
+        NavBarMutator.required = true;
+        NavBarMutator.mutate();
+      } else if (NavBarMutator.count >= 10) {
+        NavBarMutator.stopTimer();
+        NavBarMutator.required = false;
+      }
+    }
+  },
+
+  mutate: function() {
+    NavBarMutator.waitToMutate();
+  },
+
+  isReady: function() {
+    return (
+      NavBarMutator.required &&
+      $("nav").length > 0 &&
+      $("nav a.dropdown__link[data-mutated]").length == 0
+    );
+  },
+
+  waitToMutate: function() {
+    if (NavBarMutator.isReady()) {
+      NavBarMutator.stopTimer();
+      NavBarMutator.executeMutate();
+    } else {
+      NavBarMutator.startTimer(NavBarMutator.waitToMutate, 100);
+    }
+  },
+
+  executeMutate: function() {
+    $("nav a.dropdown__link[items]:not([data-mutated])").each(function(idx) {
+      var parent = $(this).parent();
+      var items = $(this).attr("items").split("|");
+      var html = "<ul class='dropdown__menu dropdown__menu__level3'>";
+      for (var i = 0; i < items.length; i += 2) {
+        var classes = "dropdown__link";
+        if (location().endsWith(items[i+1])) {
+          classes += " dropdown__link--active";
+        }
+        html += `<li><a class='${classes}' href='${items[i+1]}'>${items[i]}</a></li>`;
+      }
+      html += "</ul>";
+      parent.append(html);
+      parent.attr("class", "dropdown__submenu");
+      $(this).attr("data-mutated", true);
+    });
+    $("a").on("click", function() { NavBarMutator.mutate(); });
+  },
+
+  startTimer: function(callback, interval) {
+    NavBarMutator.stopTimer();
+    NavBarMutator.count = 0;
+    NavBarMutator.timer = setInterval(function() { 
+      NavBarMutator.count++; 
+      callback(); 
+    }, interval);
+  },
+
+  stopTimer: function() {
+    if(NavBarMutator.timer != null) {
+      clearInterval(NavBarMutator.timer);
+      NavBarMutator.timer = null;
+    }
+  },
+
 };
 
-function startMutationProcess() {
-  console.log("Starting mutation process...");
-  checkIfMutationRequired();
-}
-
-function checkIfMutationRequired() {
-  if (NAVBAR_MUTATION.required == null) {
-    console.log(`Checking if mutation required (${NAVBAR_MUTATION.count})...`);
-    if ($("nav").length == 0 && NAVBAR_MUTATION.timer == null) {
-      startMutationTimer(checkIfMutationRequired, 100);
-    } else if ($("nav a.dropdown__link[items]").length > 0) {
-      stopMutationTimer();
-      NAVBAR_MUTATION.required = true;
-      executeMutation();
-    } else if (NAVBAR_MUTATION.count >= 10) {
-      stopMutationTimer();
-      NAVBAR_MUTATION.required = false;
-    }
-  }
-}
-
-function startMutationTimer(callback, interval) {
-  stopMutationTimer();
-  NAVBAR_MUTATION.count = 0;
-  NAVBAR_MUTATION.timer = setInterval(function(){ NAVBAR_MUTATION.count++; callback(); }, interval);
-}
-
-function stopMutationTimer() {
-  if(NAVBAR_MUTATION.timer != null) {
-    clearInterval(NAVBAR_MUTATION.timer);
-    NAVBAR_MUTATION.timer = null;
-  }
-}
-
-function startPageChange() {
-  startMutationTimer(endPageChange, 500);
-}
-
-function endPageChange() {
-  stopMutationTimer();
-  waitToMutate();
-}
-
-function waitToMutate() {
-  console.log(`Waiting to mutate (${NAVBAR_MUTATION.count})...`);
-  if ($("nav").length == 0) {
-    startMutationTimer(waitToMutate, 100);
-  } else {
-    stopMutationTimer();
-    executeMutation();
-  }
-}
-
-function executeMutation() {
-  console.log(`Executing mutation...`);
-  $("nav a.dropdown__link[items]:not([data-mutated])").each(function(idx) {
-    console.log(`Mutating ${idx}!`);
-    var parent = $(this).parent();
-    var items = $(this).attr("items").split("|");
-    var html = "<ul class='dropdown__menu dropdown__menu__level3'>";
-    for (var i = 0; i < items.length; i += 2) {
-      console.log(idx + ": " + items[i] + " (" + items[i+1] + ")");
-      var classes = "dropdown__link";
-      if (location().endsWith(items[i+1])) {
-        classes += " dropdown__link--active";
-      }
-      html += `<li><a class='${classes}' href='${items[i+1]}'>${items[i]}</a></li>`;
-    }
-    html += "</ul>";
-    parent.append(html);
-    parent.attr("class", "dropdown__submenu");
-    $(this).attr("data-mutated", true);
-  });
-  $("a").on("click", function() { console.log("Page change starting..."); startPageChange(); });
-}
-
 $(function() {
-  startMutationProcess();
+  NavBarMutator.init();
 });
-
