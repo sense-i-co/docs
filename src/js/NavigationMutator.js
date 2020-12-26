56b5a1269e9a904b02a70a5e6ee03513ac86f0b8
Scott Hallauer <scott.hallauer@gmail.com>
2020-12-26 12:52:20 +0200

    Refactor and document plugin

--- src/js/NavigationMutator.js
+++ src/js/NavigationMutator.js
@@ -1,3 +1,90 @@
+/**
+ * The NavigationMutator plugin enables a third level of navigation in Docusaurus (on top of the built-in support for two levels).
+ * 
+ * INSTALLATION
+ * ------------
+ * To install the plugin, simply import this file in the docusaurus.config.js file as follows (changing the path if required):
+ * 
+ * module.exports = {
+ *   ...
+ *   clientModules: [
+ *     require.resolve('./src/js/NavigationMutator.js'),
+ *   ],
+ *   ...
+ * };
+ * 
+ * USAGE
+ * -----
+ * To use the plugin, add your level 1 and 2 navigation items as usual in the docusaurus.config.js file. For level 3 navigation
+ * items, add an 'items' array to your level 2 parent item consisting of pairs of strings representing each item's title and
+ * link respectively. After this array, remember to add '.join("|")' to concatenate this array into a single string.
+ * 
+ * Note: It is optional for level 1 and 2 navigation items to have a link (i.e. 'to' attribute) but it is required for level 3
+ * items. In other words, your level 3 items must represent actual pages that can be navigated to, whereas level 1 and 2 items
+ * can either represent actual pages or just categories (for sub-items) visible in the navigation bar. If a level 1 or 2 item
+ * has no sub-items (e.g. 'Category 2' in the example below), then it must have a link (i.e. only leaf nodes must have links).
+ * 
+ * EXAMPLE
+ * -------
+ * This example will define a 3 level navigation system with the following structure:
+ * 
+ * Example (Level 1)  <-- no link
+ * ├── Category 1 (Level 2)  <-- no link
+ * │   └── Item 1A (Level 3)
+ * │   └── Item 1B (Level 3)
+ * │   └── Item 1C (Level 3)
+ * ├── Category 2 (Level 2)  <-- link
+ * └── Category 3 (Level 2)  <-- link
+ *     └── Item 3A (Level 3)
+ *     └── Item 3B (Level 3)
+ * 
+ * The code for this structure would be in the docusaurus.config.js file as follows:
+ * 
+ * module.exports = {
+ *   ...
+ *   themeConfig: {
+ *     navbar: {
+ *       ...
+ *       items: [
+ *         {
+ *           activeBasePath: 'example',
+ *           label: 'Example (Level 1)',
+ *           position: 'left',
+ *           items: [
+ *             {
+ *               activeBasePath: 'example/category1',
+ *               label: 'Category 1 (Level 2)',
+ *               items: [
+ *                 'Item 1A (Level 3)', '/example/category1/item1a',
+ *                 'Item 1B (Level 3)', '/example/category1/item1b',
+ *                 'Item 1C (Level 3)', '/example/category1/item1c'
+ *               ].join("|")
+ *             },
+ *             {
+ *               to: 'example/category2/',
+ *               activeBasePath: 'example/category2',
+ *               label: 'Category 2 (Level 2)',
+ *             },
+ *             {
+ *               to: 'example/category3/',
+ *               activeBasePath: 'example/category3',
+ *               label: 'Category 3 (Level 2)',
+ *               items: [
+ *                 'Item 3A (Level 3)', '/example/category3/item3a',
+ *                 'Item 3B (Level 3)', '/example/category3/item3a'
+ *               ].join("|")
+ *             }
+ *           ]
+ *         },
+ *       ],
+ *     },
+ *     ...
+ *   },
+ *   ...
+ * };
+ * 
+ */
+
 import $ from "jquery";
 import location from 'location-href'; 
 
@@ -5,26 +92,32 @@ class NavigationMutator {
 
   constructor() {
 
-    // CONSTANTS
-    this.SIDEBAR_ITEM_HEIGHT = 36;
-    this.SIDEBAR_LIST_PADDING = 4;
+    // NAVBAR CONSTANTS
     this.NAVBAR_SUBMENU_CLASS = "dropdown__menu dropdown__submenu__list";
     this.NAVBAR_LINK_CLASS = "dropdown__link";
-    this.NAVBAR_LINK_ACTIVE_PATH_CLASS = "navbar__link--active"; // class for link representing parent page (i.e. active base path)
-    this.NAVBAR_LINK_CURRENT_PAGE_CLASS = "dropdown__link--active"; // class for link representing current page (i.e. active page)
-    this.SIDEBAR_SUBMENU_CLASS = "menu__list";
-    this.SIDEBAR_LINK_CLASS = "menu__link";
-    this.SIDEBAR_LINK_ACTIVE_PATH_CLASS = this.NAVBAR_LINK_ACTIVE_PATH_CLASS; // class for link representing parent page (i.e. active base path)
-    this.SIDEBAR_LINK_CURRENT_PAGE_CLASS = "menu__link--active"; // class for link representing current page (i.e. active page)
-    this.SIDEBAR_LINK_COLLAPSED_CLASS = "menu__list-item--collapsed";
+    this.NAVBAR_LINK_ACTIVE_PATH_CLASS = "navbar__link--active"; // class for link representing parent of currently selected page (i.e. active base path)
+    this.NAVBAR_LINK_CURRENT_PAGE_CLASS = "dropdown__link--active"; // class for link representing currently selected page (i.e. active page)
     this.NAVBAR_SELECTOR = "nav.navbar";
     this.NAVBAR_EXPANDABLE_LINK_SELECTOR = `${this.NAVBAR_SELECTOR} a.${this.NAVBAR_LINK_CLASS}[items]`;
+
+    // SIDEBAR CONSTANTS
+    this.SIDEBAR_ITEM_HEIGHT = 36; // height in pixels of items in the sidebar (used to calculate height changes on expand/collapse of items)
+    this.SIDEBAR_LIST_PADDING = 4; // padding in pixels on top and bottom of sublists in the sidebar (also used in height change calculations)
+    this.SIDEBAR_SUBMENU_CLASS = "menu__list";
+    this.SIDEBAR_LINK_CLASS = "menu__link";
+    this.SIDEBAR_LINK_ACTIVE_PATH_CLASS = this.NAVBAR_LINK_ACTIVE_PATH_CLASS;
+    this.SIDEBAR_LINK_CURRENT_PAGE_CLASS = "menu__link--active";
+    this.SIDEBAR_LINK_COLLAPSED_CLASS = "menu__list-item--collapsed";
     this.SIDEBAR_SELECTOR = "div.navbar-sidebar";
     this.SIDEBAR_EXPANDABLE_LINK_SELECTOR = `${this.SIDEBAR_SELECTOR} a.${this.SIDEBAR_LINK_CLASS}[items]`;
-    this.MUTATED_FLAG = "data-mutated";
 
-    // CSS
+    // OTHER CONSTANTS
+    this.MUTATED_FLAG = "data-mutated"; // attribute to apply to links once they are mutated (indicating that mutation does not need to be executed again)
+
+    // CSS CONSTANT
     this.STYLES = `
+      /* NAVBAR STYLES */
+      /* display expand arrow to right of dropdown level 2 links that have level 3 sub-items */
       a.dropdown__link[items]::after{
         background: var(--ifm-menu-link-sublist-icon) 50% / 2rem 2rem;
         content: ' ';
@@ -37,6 +130,7 @@ class NavigationMutator {
         transform: rotate(90deg);
       }
     
+      /* position level 3 submenu to the right of level 2 parent link */
       ul.dropdown__submenu__list {
         left: 100%;
         top: auto;
@@ -47,12 +141,15 @@ class NavigationMutator {
         transform: scale(.8) !important;
       }
     
+      /* display level 3 submenu of hover of level 2 parent link or the submenu itself */
       li.dropdown__submenu__parent:hover ul.dropdown__submenu__list, ul.dropdown__submenu__list:hover {
         opacity: 1 !important;
         transform: scale(1) !important;
         visibility: visible !important;
       }
 
+      /* SIDEBAR STYLES */
+      /* apply default styles to all active items not just those that are not(.menu__link--sublist) */
       .menu__link--active {
         background: var(--ifm-menu-color-background-active);
       }
@@ -64,6 +161,7 @@ class NavigationMutator {
 
   }
 
+  // check if there is any level 3 navigation on this page (wait up 1 second) and, if there is, inject styles and execute mutator
   init() {
     if($(`${this.NAVBAR_EXPANDABLE_LINK_SELECTOR}, ${this.SIDEBAR_EXPANDABLE_LINK_SELECTOR}`).length > 0) {
       this.ready = true;
@@ -73,12 +171,13 @@ class NavigationMutator {
     }
     if(this.ready){
       if($("#nav-level-3-styles").length == 0) {
-        $("body").append(`<style id="nav-level-3-styles" type="text/css">${this.STYLES}</style>`); // inject CSS styles
+        $("body").append(`<style id="nav-level-3-styles" type="text/css">${this.STYLES}</style>`); // inject CSS styles if not present
       }
       this.wait();
     }
   }
 
+  // wait until there are no mutated links (i.e. old page unload complete) and the navbar is present (i.e. new page load complete) to mutate the navigation system
   wait() {
     if (
       this.ready &&
@@ -91,23 +190,26 @@ class NavigationMutator {
     }
   }
 
+  // mutate both the navbar (desktop) and sidebar (mobile) to display level 3 navigation links
   mutate() {
 
     var mutator = this;
 
-    var getMenuHTML = function(type, parent, items) {
+    // get the HTML for the level 3 list to be inserted into either the navbar or sidebar
+    var getListL3HTML = function(type, linkL2, linkInfoL3) {
       var isNavBar = (type == "navbar");
       var html = `<ul class='${isNavBar ? mutator.NAVBAR_SUBMENU_CLASS : mutator.SIDEBAR_SUBMENU_CLASS}'>`;
-      for(var i = 0; i < items.length; i += 2) {
-        var title = items[i];
-        var href = items[i+1];
+      for(var i = 0; i < linkInfoL3.length; i += 2) {
+        var title = linkInfoL3[i];
+        var href = linkInfoL3[i+1];
         var classes = (isNavBar ? mutator.NAVBAR_LINK_CLASS : mutator.SIDEBAR_LINK_CLASS);
         if(location().endsWith(href)) {
           classes += ` ${isNavBar ? mutator.NAVBAR_LINK_CURRENT_PAGE_CLASS : mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS}`;
-          parent.addClass(isNavBar ? mutator.NAVBAR_LINK_ACTIVE_PATH_CLASS : mutator.SIDEBAR_LINK_ACTIVE_PATH_CLASS);
-          parent.removeClass(isNavBar ? mutator.NAVBAR_LINK_CURRENT_PAGE_CLASS : mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS);
+          linkL2.addClass(isNavBar ? mutator.NAVBAR_LINK_ACTIVE_PATH_CLASS : mutator.SIDEBAR_LINK_ACTIVE_PATH_CLASS);
+          linkL2.removeClass(isNavBar ? mutator.NAVBAR_LINK_CURRENT_PAGE_CLASS : mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS);
           if(!isNavBar) {
-            parent.closest("ul").prev("a").addClass(mutator.NAVBAR_LINK_ACTIVE_PATH_CLASS);
+            var linkL1 = linkL2.closest("ul").prev("a");
+            linkL1.addClass(mutator.NAVBAR_LINK_ACTIVE_PATH_CLASS);
           }
         }
         html += `<li ${isNavBar ? "" : "class='menu__list-item'"}><a class='${classes}' href='${href}'>${title}</a></li>`;
@@ -116,115 +218,118 @@ class NavigationMutator {
       return html;
     }
 
-    var getNumVisibleItems = function(list) {
-      if(list.parent().hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
+    // get the number of items in a level 2 list and its sublists (level 3) that are currently visible
+    var getListL2NumVisibleItems = function(listL2) {
+      if(listL2.parent().hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
         return 0;
       } else {
-        var items = list.children("li");
+        var itemsL3 = listL2.children("li");
         var count = 0;
-        items.each(function() {
+        itemsL3.each(function() {
+          var itemL3 = $(this);
           count++;
-          var sublist = $(this).children("ul");
-          if(!$(this).hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS) && sublist.length == 1) {
-            count += getNumVisibleItems(sublist);
+          var listL3 = itemL3.children("ul");
+          if(!itemL3.hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS) && listL3.length == 1) {
+            count += getListL2NumVisibleItems(listL3);
           }
         });
         return count;
       }
     }
 
-    var toggleLevel1Link = function(link) {
-      var container = link.parent();
-      var toplist = $(`div.menu > ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
-      var midlist = link.next("ul");
-      if(container.hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
-        container.removeClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
-        var numItems = getNumVisibleItems(midlist);
-        console.log(`Visible Items: ${numItems}`);
-        var heightChange = (numItems * mutator.SIDEBAR_ITEM_HEIGHT + mutator.SIDEBAR_LIST_PADDING);
-        toplist.css("height", (toplist.height() + heightChange) + "px");
-        midlist.css("height", (midlist.height() + heightChange) + "px");
-      } else {
-        container.addClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
-        var numItems = getNumVisibleItems(midlist);
-        console.log(`Visible Items: ${numItems}`);
-        var heightChange = (numItems * mutator.SIDEBAR_ITEM_HEIGHT + mutator.SIDEBAR_LIST_PADDING);
-        toplist.css("height", (toplist.height() - heightChange) + "px");
-        midlist.css("height", (midlist.height() - heightChange) + "px");
-      }
-    }
-
     // NAVBAR MUTATION (DESKTOP)
-    $(`${mutator.NAVBAR_EXPANDABLE_LINK_SELECTOR}:not([${mutator.MUTATED_FLAG}])`).each(function(idx) {
+    $(`${mutator.NAVBAR_EXPANDABLE_LINK_SELECTOR}:not([${mutator.MUTATED_FLAG}])`).each(function() {
 
-      var container = $(this).parent();
-      var parentLink = $(this);
-      var childLinks = $(this).attr("items").split("|");
+      var itemL2 = $(this).parent();
+      var linkL2 = $(this);
+      var linkInfoL3 = $(this).attr("items").split("|");
 
-      container.append(getMenuHTML("navbar", parentLink, childLinks));
-      container.addClass("dropdown__submenu__parent");
+      itemL2.append(getListL3HTML("navbar", linkL2, linkInfoL3));
+      itemL2.addClass("dropdown__submenu__parent");
 
-      parentLink.attr(mutator.MUTATED_FLAG, true);
+      linkL2.attr(mutator.MUTATED_FLAG, true);
 
     });
 
     // SIDEBAR MUTATION (MOBILE)
-    $(`${mutator.SIDEBAR_EXPANDABLE_LINK_SELECTOR}:not([${mutator.MUTATED_FLAG}])`).each(function(idx) {
+    $(`${mutator.SIDEBAR_EXPANDABLE_LINK_SELECTOR}:not([${mutator.MUTATED_FLAG}])`).each(function() {
 
-      var container = $(this).parent();
-      var parentLink = $(this);
-      var childLinks = $(this).attr("items").split("|");
+      var itemL2 = $(this).parent();
+      var linkL1 = $(this).closest("ul").prev("a");
+      var linkL2 = $(this);
+      var linkInfoL3 = $(this).attr("items").split("|");
 
-      var level1Link = parentLink.closest("ul").prev("a");
-      if(location().endsWith(level1Link.attr("href"))) {
-        level1Link.addClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS);
+      if(location().endsWith(linkL1.attr("href"))) {
+        linkL1.addClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS);
       }
       
-      container.append(getMenuHTML("sidebar", parentLink, childLinks));
-      container.addClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
+      itemL2.append(getListL3HTML("sidebar", linkL2, linkInfoL3));
+      itemL2.addClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
 
-      parentLink.on("click", function() {
-        var toplist = $(`div.menu > ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
-        var midlist = container.parent();
-        var sublist = container.find(`ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
-        var heightChange = ((childLinks.length/2) * mutator.SIDEBAR_ITEM_HEIGHT + mutator.SIDEBAR_LIST_PADDING);
-        if(container.hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
-          container.removeClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
-          toplist.css("height", (toplist.height() + heightChange) + "px");
-          midlist.css("height", (midlist.height() + heightChange) + "px");
-          sublist.css("height", heightChange + "px");
+      // expand/collapse a level 2 link in the sidebar
+      linkL2.on("click", function() {
+        var listL1 = $(`div.menu > ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
+        var listL2 = itemL2.parent();
+        var listL3 = itemL2.find(`ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
+        var heightChange = ((linkInfoL3.length/2) * mutator.SIDEBAR_ITEM_HEIGHT + mutator.SIDEBAR_LIST_PADDING);
+        itemL2.toggleClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
+        if(itemL2.hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
+          listL1.css("height", (listL1.height() - heightChange) + "px");
+          listL2.css("height", (listL2.height() - heightChange) + "px");
+          listL3.css("height", "");
         } else {
-          container.addClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
-          toplist.css("height", (toplist.height() - heightChange) + "px");
-          midlist.css("height", (midlist.height() - heightChange) + "px");
-          sublist.css("height", "");
+          listL1.css("height", (listL1.height() + heightChange) + "px");
+          listL2.css("height", (listL2.height() + heightChange) + "px");
+          listL3.css("height", heightChange + "px");
         }
-        
-        if(parentLink.hasClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS) || !parentLink.is("[href]")) { 
+        if(linkL2.hasClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS) || !linkL2.is("[href]")) { 
           return false; 
         } 
       });
 
-      parentLink.addClass("menu__link--sublist");
-      parentLink.attr(mutator.MUTATED_FLAG, true);
+      linkL2.addClass("menu__link--sublist");
+      linkL2.attr(mutator.MUTATED_FLAG, true);
 
     });
+
+    // expand/collapse a level 1 link in the sidebar
     $("div.menu > ul > li > ul").prev("a").on("click", function() {
-      if($(this).hasClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS) || !$(this).is("[href]")) { 
-        toggleLevel1Link($(this));
+      var linkL1 = $(this);
+      if(linkL1.hasClass(mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS) || !linkL1.is("[href]")) {
+        var itemL1 = linkL1.parent();
+        var listL1 = $(`div.menu > ul.${mutator.SIDEBAR_SUBMENU_CLASS}`);
+        var listL2 = linkL1.next("ul");
+        itemL1.toggleClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
+        var numItems = getListL2NumVisibleItems(listL2);
+        var heightChange = (numItems * mutator.SIDEBAR_ITEM_HEIGHT + mutator.SIDEBAR_LIST_PADDING);
+        if(itemL1.hasClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS)) {
+          listL1.css("height", (listL1.height() - heightChange) + "px");
+          listL2.css("height", (listL2.height() - heightChange) + "px");
+        } else {
+          listL1.css("height", (listL1.height() + heightChange) + "px");
+          listL2.css("height", (listL2.height() + heightChange) + "px");
+        }
         return false;
       }
     });
-    $(`a.${mutator.SIDEBAR_LINK_ACTIVE_PATH_CLASS}, a.${mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS}`).parents(`li.${mutator.SIDEBAR_LINK_COLLAPSED_CLASS}`).removeClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
+
+    // expand all parent links of an active link in the sidebar on page load (i.e. display breadcrumb trail)
+    $(`a.${mutator.SIDEBAR_LINK_ACTIVE_PATH_CLASS}, a.${mutator.SIDEBAR_LINK_CURRENT_PAGE_CLASS}`)
+      .parents(`li.${mutator.SIDEBAR_LINK_COLLAPSED_CLASS}`)
+      .removeClass(mutator.SIDEBAR_LINK_COLLAPSED_CLASS);
+
+    // set the height of all menus in the sidebar on page load (i.e. initialise heights for expand/collapse animations)
     $(`ul.${mutator.SIDEBAR_SUBMENU_CLASS}`).each(function(idx) {
       $(this).css("height", $(this).height());
     });
 
+    // bind all navigation clicks to call the navigation mutator (i.e. update navbar and sidebar on react page change)
     $("a").on("click", function() { mutator.wait(); });
   }
 
 }
 
+// run the navigation mutator on page load
 $(function() {
   var mutator = new NavigationMutator();
   mutator.init();
