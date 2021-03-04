d98a0108e5e6f3dac2f07e6d94bf18a25c793314
Scott Hallauer <scott.hallauer@gmail.com>
2021-03-04 17:36:40 +0200

    Add first draft of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
+import ImageMap from "../components/ImageMap";
+import ImageDeck from "../components/ImageDeck";
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
-
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_researching_22gp.svg',
-    description: (
-      <>
-        Sense-i was designed from the ground up to be quick to install and easy 
-        to use to get your business running on a single platform faster than any 
-        other solution available to small and medium sized companies.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_dev_focus_b9xo.svg',
-    description: (
-      <>
-        Clear job descriptions and user profiles with easy to understand reports 
-        keeps everyone in your team focused on the priority tasks for the day. 
-        This leads to faster processing of orders, reduce overheads and greater 
-        sales and customer satisfaction.
-      </>
-    ),
-  },
-  {
-    title: 'Real-time Control',
-    imageUrl: 'img/undraw_real-time_sync_o57k.svg',
-    description: (
-      <>
-        Powerful reporting tools give owners and managers access to up-to-date, 
-        accurate and comprehensive information about every transaction and aspect 
-        of your business so that you can monitor the business performance in 
-        real-time.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
 
 function Home() {
   const context = useDocusaurusContext();
@@ -65,48 +12,94 @@ function Home() {
   return (
     <Layout
       description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <img src={useBaseUrl("/img/text_logo_white.png")} className={styles.titleLogoImage}/>
-          <p className={clsx("hero__subtitle", styles.subtitleText)}>{siteConfig.tagline}</p>
-          <div className={clsx('home-page-button-container', styles.buttons)}>
-            <Link
-              className={clsx(
-                'button button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              View Documentation
-            </Link>
-          </div>
-          {
-          /* Temporarily hide the download button until the download page is finalised */
-          /*
-          <div className={clsx('home-page-button-container', styles.buttons)}>
-            <Link
-              className={clsx(
-                'button button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to='https://elfworks.co.za'>
-              Download Application
-            </Link>
-          </div>
-          */}
-        </div>
-      </header>
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
+        <div className={styles.heroContainer}>
+          <div className={styles.heroTitleWrapper}>
+            <div className={styles.heroTitle}>
+              Take control and build your successful business with Sense-i
             </div>
-          </section>
-        )}
+            <div className={styles.heroSubtitle}>
+              Building a business can be hard, complicated and full of risks… that’s why we built Sense-i, a complete program to help you grow a business that works.
+            </div>
+          </div>
+          <img className={styles.heroBanner} src={useBaseUrl("/img/pages/index/hero-banner.png")}/>
+        </div>
+        <div className={styles.headlineWrapper}>
+          <div className={styles.headline}>
+            Over 20 years, leading SME’s have used Sense-i to build successful, well-run, profitable companies
+          </div>
+        </div>
+        <ImageDeck 
+          images={[
+            {src: "/img/pages/index/ergoform-testimonial.png"},
+            {src: "/img/pages/index/ergoform-testimonial.png"}
+          ]}
+          options={{
+            buttons: false
+          }}
+        />
+        <ImageMap
+          src="/img/pages/index/logo-collage.png"
+          maxWidth="1500px"
+          areas={[
+            {
+              link: "https://www.sheetstreet.com",
+              boundary: {
+                topLeft: [51, 78],
+                bottomRight: [415, 170]
+              }
+            },
+            {
+              link: "https://wunders.co.za",
+              boundary: {
+                topLeft: [515, 73],
+                bottomRight: [872, 165]
+              }
+            },
+            {
+              link: "http://www.codesigns.co.za",
+              boundary: {
+                topLeft: [968, 68],
+                bottomRight: [1328, 180]
+              }
+            },
+            {
+              link: "https://bedbathhome.co.za",
+              boundary: {
+                topLeft: [1436, 87],
+                bottomRight: [1793, 162]
+              }
+            },
+            {
+              link: "https://pierrecronje.co.za",
+              boundary: {
+                topLeft: [60, 284],
+                bottomRight: [582, 382]
+              }
+            },
+            {
+              link: "http://www.bambisana.co.za",
+              boundary: {
+                topLeft: [690, 222],
+                bottomRight: [1090, 450]
+              }
+            },
+            {
+              link: "https://ergoform.co.za",
+              boundary: {
+                topLeft: [1263, 280],
+                bottomRight: [1402, 414]
+              }
+            },
+            {
+              link: "https://inksaver.co.za",
+              boundary: {
+                topLeft: [1542, 290],
+                bottomRight: [1906, 385]
+              }
+            }
+          ]}
+        />
       </main>
     </Layout>
   );
