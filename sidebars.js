ab260df7016162874a791f5b5e12b20e37055e75
Kobus67 <kobus@drumbeat.co.za>
2024-02-19 11:39:48 +0200

    Update Sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -91,7 +91,7 @@ const sidebars= {
                 items: [
                   "DPMNT-001",
 			            "SAF-1204",
-                  "CNF-002",				
+                  "CNF-001",				
 				          "SAF-441",
                 ],   
 			        },
